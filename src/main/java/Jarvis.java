8511c7275edb8516a8ea99cbc4b01f4fee719d43
Koi Yim <e1527479@u.nus.edu>
2026-08-15 13:09:51 +0800

    Updated the initial code to greet the user and exit

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -2,7 +2,7 @@
  * Runs the Jarvis chatbot and displays its welcome and goodbye messages.
  */
 public class Jarvis {
-    private static String banner;
+    private static String banner, borderLine, initialMsg;
 
     public static void main(String[] args) {
         banner =
@@ -13,6 +13,16 @@ public class Jarvis {
                 + "╚█████╔╝██║  ██║██║  ██║ ╚████╔╝ ██║███████║\n"
                 + " ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚══════╝\n";
 
-        System.out.println(banner);
+        borderLine = "____________________________________________________________\n";
+
+        initialMsg = borderLine
+                + banner
+                + "Hello, I am Jarvis, your friendly AI assistant!\n"
+                + "What can I do for you ?\n"
+                + borderLine
+                + "Goodbye, I await for your next visit !\n"
+                + borderLine;
+
+        System.out.println(initialMsg);
     }
 }
