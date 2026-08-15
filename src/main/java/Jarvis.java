4173a7e563617a514371aac76f7e77f845585254
Koi Yim <e1527479@u.nus.edu>
2026-08-15 14:16:06 +0800

    Updated code to echo commands and changed greeting

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,10 +1,16 @@
 /**
  * Runs the Jarvis chatbot and displays its welcome and goodbye messages.
  */
+
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.Scanner;
+
 public class Jarvis {
-    private static String banner, borderLine, initialMsg;
+    private static String banner, borderLine, initialMsg, exitMsg, clipThatMsg;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         banner =
                   "     ██╗ █████╗ ██████╗ ██╗   ██╗██╗███████╗\n"
                 + "     ██║██╔══██╗██╔══██╗██║   ██║██║██╔════╝\n"
@@ -17,12 +23,37 @@ public class Jarvis {
 
         initialMsg = borderLine
                 + banner
-                + "Hello, I am Jarvis, your friendly AI assistant!\n"
-                + "What can I do for you ?\n"
-                + borderLine
-                + "Goodbye, I await for your next visit !\n"
+                + "Good day Sir/Ma' am, I am Jarvis, your friendly AI assistant\n"
+                + "How may I be of service to you today ?\n"
                 + borderLine;
 
+        exitMsg = borderLine
+                + "Goodbye Sir/Ma' am. I hope to be of service to you again next time\n"
+                + borderLine;
+
+        clipThatMsg = borderLine
+                    + "Clipped and Ready to ship Sir\n"
+                    + borderLine;
+
         System.out.println(initialMsg);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            switch(userInput) {
+                case "bye":
+                    System.out.println(exitMsg);
+                    break;
+                case "Jarvis, clip that":
+                    System.out.println(clipThatMsg);
+                    break;
+                default:
+                    System.out.println(userInput+"\n");
+            }
+
+            if(userInput.equals("bye")) {
+                break;
+            }
+        }
     }
 }
