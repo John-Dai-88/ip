1cdf9ae7baece43073469740523a9b7afbfa9418
Koi Yim <e1527479@u.nus.edu>
2026-08-16 01:06:46 +0800

    Changed file name of exception from InvalidDeadlineException to InvalidDateAndTimeException

---
+++ src/main/java/Exceptions/InvalidDateAndTimeException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class InvalidDateAndTimeException extends  JarvisException {
+
+    public InvalidDateAndTimeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/Exceptions/InvalidDeadlineException.java
+++
@@ -1,8 +0,0 @@
-package Exceptions;
-
-public class InvalidDeadlineException extends  JarvisException {
-
-    public InvalidDeadlineException(String message) {
-        super(message);
-    }
-}
