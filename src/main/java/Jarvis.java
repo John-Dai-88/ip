57d9663af5fc4ae728ef4a28920072e0596d2c9d
Koi Yim <e1527479@u.nus.edu>
2026-08-15 13:05:01 +0800

    Renamed Chatbot and removed all traces of Duke from the code

---
+++ src/main/java/Jarvis.java
@@ -0,0 +1,18 @@
+/**
+ * Runs the Jarvis chatbot and displays its welcome and goodbye messages.
+ */
+public class Jarvis {
+    private static String banner;
+
+    public static void main(String[] args) {
+        banner =
+                  "     ██╗ █████╗ ██████╗ ██╗   ██╗██╗███████╗\n"
+                + "     ██║██╔══██╗██╔══██╗██║   ██║██║██╔════╝\n"
+                + "     ██║███████║██████╔╝██║   ██║██║███████╗\n"
+                + "██   ██║██╔══██║██╔══██╗╚██╗ ██╔╝██║╚════██║\n"
+                + "╚█████╔╝██║  ██║██║  ██║ ╚████╔╝ ██║███████║\n"
+                + " ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚══════╝\n";
+
+        System.out.println(banner);
+    }
+}
