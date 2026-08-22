9ee4d6548873aed74135a6d0cc6439b3306e71a9
Koi Yim <e1527479@u.nus.edu>
2026-08-22 21:42:05 +0800

    Add JavaDoc for Parser class

--- src/main/java/jarvis/ui/Parser.java
+++ src/main/java/jarvis/ui/Parser.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/** Parses user commands and converts them into tasks or command parameters. */
 public class Parser {
 
     private static final String BORDER_LINE =
