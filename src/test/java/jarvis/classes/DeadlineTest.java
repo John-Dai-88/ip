694ad444396a5208ba8fdd0382c2e1eae33e2ebb
Koi Yim <e1527479@u.nus.edu>
2026-08-23 01:08:34 +0800

    Add more tests for Deadline.java

--- src/test/java/jarvis/classes/DeadlineTest.java
+++ src/test/java/jarvis/classes/DeadlineTest.java
@@ -11,17 +11,63 @@ public class DeadlineTest {
     /** Verifies that a deadline with a date only formats correctly. */
     @Test
     public void constructor_dateOnlyDeadlineFormatsCorrectly() {
-        Deadline deadline = new Deadline("Submit report", LocalDateTime.of(2026, 8, 22, 0, 0));
+        Deadline deadline = new Deadline("Read book", LocalDateTime.of(2026, 8, 22, 0, 0));
 
         assertEquals(Task.CompletionStatus.UNDONE, deadline.getStatus());
-        assertEquals("[D][] Submit report (by: 08 22 2026)", deadline.toString());
+        assertEquals("[D][] Read book (by: 08 22 2026)", deadline.toString());
+    }
+
+    /** Verifies that a date only deadline can be marked as done and displays the done marker. */
+    @Test
+    public void setCompletionStatus_doneDateOnlyDeadlineDisplaysDoneMarker() {
+        Deadline deadline = new Deadline("Read book", LocalDateTime.of(2026, 8, 22, 0, 0));
+
+        deadline.setCompletionStatus(Task.CompletionStatus.DONE);
+
+        assertEquals(Task.CompletionStatus.DONE, deadline.getStatus());
+        assertEquals("[D][X] Read book (by: 08 22 2026)", deadline.toString());
+    }
+
+    /** Verifies that a date only completed deadline can be marked undone again. */
+    @Test
+    public void setCompletionStatus_undoneDateOnlyDeadlineDisplaysUndoneMarker() {
+        Deadline deadline = new Deadline("Read book", LocalDateTime.of(2026, 8, 22, 0, 0));
+
+        deadline.setCompletionStatus(Task.CompletionStatus.DONE);
+        deadline.setCompletionStatus(Task.CompletionStatus.UNDONE);
+
+        assertEquals(Task.CompletionStatus.UNDONE, deadline.getStatus());
+        assertEquals("[D][] Read book (by: 08 22 2026)", deadline.toString());
     }
 
     /** Verifies that a deadline with a time formats correctly. */
     @Test
-    public void constructor_deadlineWithTimeFormatsCorrectly() {
-        Deadline deadline = new Deadline("Submit report", LocalDateTime.of(2026, 8, 22, 23, 45));
+    public void constructor_dateAndTimeDeadlineFormatsCorrectly() {
+        Deadline deadline = new Deadline("Read book", LocalDateTime.of(2026, 8, 22, 23, 45));
 
-        assertEquals("[D][] Submit report (by: 08 22 2026 23:45)", deadline.toString());
+        assertEquals("[D][] Read book (by: 08 22 2026 23:45)", deadline.toString());
+    }
+
+    /** Verifies that a date and time deadline can be marked as done and displays the done marker. */
+    @Test
+    public void setCompletionStatus_doneDateAndTimeDeadlineDisplaysDoneMarker() {
+        Deadline deadline = new Deadline("Read book", LocalDateTime.of(2026, 8, 22, 23, 45));
+
+        deadline.setCompletionStatus(Task.CompletionStatus.DONE);
+
+        assertEquals(Task.CompletionStatus.DONE, deadline.getStatus());
+        assertEquals("[D][X] Read book (by: 08 22 2026 23:45)", deadline.toString());
+    }
+
+    /** Verifies that a completed date and time data can be marked undone again. */
+    @Test
+    public void setCompletionStatus_undoneDateAndTimeDeadlineDisplaysUndoneMarker() {
+        Deadline deadline = new Deadline("Read book", LocalDateTime.of(2026, 8, 22, 23, 45));
+
+        deadline.setCompletionStatus(Task.CompletionStatus.DONE);
+        deadline.setCompletionStatus(Task.CompletionStatus.UNDONE);
+
+        assertEquals(Task.CompletionStatus.UNDONE, deadline.getStatus());
+        assertEquals("[D][] Read book (by: 08 22 2026 23:45)", deadline.toString());
     }
 }
