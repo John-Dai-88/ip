944b8b3630932d67060490e67117b40e35d02665
Koi Yim <e1527479@u.nus.edu>
2026-08-22 23:47:14 +0800

    Create new unit test classes

---
+++ src/test/java/jarvis/classes/DeadlineTest.java
@@ -0,0 +1,4 @@
+package jarvis.classes;
+
+public class DeadlineTest {
+}
---
+++ src/test/java/jarvis/classes/TaskTest.java
@@ -0,0 +1,40 @@
+package jarvis.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** Tests the behavior of {@link Task}. */
+public class TaskTest {
+    /** Verifies that a newly created task is undone and formatted correctly. */
+    @Test
+    public void constructor_createsUndoneTask() {
+        Task task = new Task("Read book");
+
+        assertEquals(Task.CompletionStatus.UNDONE, task.getStatus());
+        assertEquals("[] Read book", task.toString());
+    }
+
+    /** Verifies that a task can be marked as done and displays the done marker. */
+    @Test
+    public void setCompletionStatus_doneTaskDisplaysDoneMarker() {
+        Task task = new Task("Read book");
+
+        task.setCompletionStatus(Task.CompletionStatus.DONE);
+
+        assertEquals(Task.CompletionStatus.DONE, task.getStatus());
+        assertEquals("[X] Read book", task.toString());
+    }
+
+    /** Verifies that a completed task can be marked undone again. */
+    @Test
+    public void setCompletionStatus_undoneTaskDisplaysUndoneMarker() {
+        Task task = new Task("Read book");
+        task.setCompletionStatus(Task.CompletionStatus.DONE);
+
+        task.setCompletionStatus(Task.CompletionStatus.UNDONE);
+
+        assertEquals(Task.CompletionStatus.UNDONE, task.getStatus());
+        assertEquals("[] Read book", task.toString());
+    }
+}
