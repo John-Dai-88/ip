27de88655107d9f92184123b0d9c2b5029b2bf8e
Koi Yim <e1527479@u.nus.edu>
2026-08-29 23:20:50 +0800

    Implement launcher class for GUI

---
+++ src/main/java/jarvis/ui/GuiLauncher.java
@@ -0,0 +1,10 @@
+package jarvis.ui;
+
+/**
+ * Launcher class to avoid JavaFX module issues.
+ */
+public class GuiLauncher {
+    public static void main(String[] args) {
+        JarvisGuiApplication.main(args);
+    }
+}
